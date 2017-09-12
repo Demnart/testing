6b0f3e74504b2f6e389ed19696f742de009b47da
genjo <insidia93@gmail.com>
2017-09-12 16:28:58 +0300

    Add ErrorController

---
+++ controllers/ErrorController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: genjo
+ * Date: 12.09.17
+ * Time: 16:27
+ */
+
+namespace app\controllers;
+
+
+class ErrorController
+extends AppController
+{
+
+    public function actionError()
+    {
+        return $this->render('error');
+    }
+}
