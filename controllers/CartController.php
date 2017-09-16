f5015d1272ef9d1e16288fa532b00c7db86065a8
Genjo <insidia93@gmail.com>
2017-09-16 16:47:44 +0300

    A controller was added to add the product to the bottle.

---
+++ controllers/CartController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Genjo
+ * Date: 14.09.2017
+ * Time: 21:05
+ */
+
+namespace app\controllers;
+
+
+class CartController
+extends AppController
+{
+    public function actionAddCart()
+    {
+        $id = \Yii::$app->request->get('id');
+        debug($id);
+    }
+
+}
