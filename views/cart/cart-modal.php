c8a09107bb00a67669616d66ae2d17032311afb3
Genjo <insidia93@gmail.com>
2017-09-16 18:51:27 +0300

    Created modal view

--- views/cart/cart-modal.php
+++ views/cart/cart-modal.php
@@ -1,3 +1,38 @@
+<?php
+if (!empty($session['cart'])): ?>
+    <div class="table-responsive">
+        <table class="table table-hover table-striped">
+            <thead>
+                <tr>
+                    <th>Изображение</th>
+                    <th>Название</th>
+                    <th>Количество</th>
+                    <th>Цена</th>
+                    <th><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($session['cart'] as $id => $item):?>
+                    <tr>
+                        <td><?= $item['img']?></td>
+                        <td><?= $item['name']?></td>
+                        <td><?= $item['qty']?></td>
+                        <td><?= $item['price']?></td>
+                        <td<span class="glyphicon glyphicon-remove text-danger del-item"  aria-hidden="true"></span></td>
+                    </tr>
+                    <tr>
+                        <td>Итого: </td>
+                        <td colspan="4"><?= $session['cart.qty']?></td>
 
-
+                    <tr>
+                        <td>На сумму: </td>
+                        <td colspan="4"><?= $session['cart.sum']?></td>
+                    </tr>
+                 <?php endforeach;?>
+            </tbody>
+        </table>
+    </div>
+    <?php else:?>
+    <h3>Корзина пуста</h3>
+<?php endif;?>
 
