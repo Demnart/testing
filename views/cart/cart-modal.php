4eaaefa52a7b365fae12bccffd54a8467d81e883
Genjo <insidia93@gmail.com>
2017-09-17 18:12:53 +0300

    added data-id to send the id of the item to be deleted

--- views/cart/cart-modal.php
+++ views/cart/cart-modal.php
@@ -18,7 +18,7 @@ if (!empty($session['cart'])): ?>
                         <td><?= $item['name']?></td>
                         <td><?= $item['qty']?></td>
                         <td><?= $item['price']?></td>
-                        <td><span class="glyphicon glyphicon-remove text-danger del-item"  aria-hidden="true"></span></td>
+                        <td><span class="glyphicon glyphicon-remove text-danger del-item" data-id="<?= $id;?>" aria-hidden="true"></span></td>
                     </tr>
                 <?php endforeach;?>
                     <tr>
