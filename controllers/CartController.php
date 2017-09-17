76f14d7b6e089cfa18a1d49263ff847692115bee
Genjo <insidia93@gmail.com>
2017-09-17 18:10:09 +0300

    Created an action to remove 1 item from the shopping cart

--- controllers/CartController.php
+++ controllers/CartController.php
@@ -41,4 +41,15 @@ extends AppController
         $this->layout = false;
         return $this->render('cart-modal',compact('session'));
     }
+
+    public function actionClearOne()
+    {
+        $id = \Yii::$app->request->get('id');
+        $session = \Yii::$app->session;
+        $session->open();
+        $cart = new Cart();
+        $cart->removeItem($id);
+        $this->layout = false;
+        return $this->render('cart-modal',compact('session'));
+    }
 }
