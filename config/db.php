50ea0a872daa14147263f6d5812e3510cc310c97
genjo <insidia93@gmail.com>
2017-09-19 10:22:37 +0300

    1

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2_loc',
     'username' => 'root',
-    'password' => '',
+    'password' => 'death9393',
     'charset' => 'utf8',
 ];
