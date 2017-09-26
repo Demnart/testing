bcdc316890f4f0ba966fece260b8c886f9029cd8
Genjo <insidia93@gmail.com>
2017-09-26 17:49:52 +0300

    Created products cart view

--- views/cart/view.php
+++ views/cart/view.php
@@ -1,1 +1,58 @@
-Корзина
+<?php
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\widgets\ActiveForm;
+?>
+
+<div class="container">
+
+    <?php
+
+    if (!empty($session['cart'])): ?>
+        <div class="table-responsive">
+            <table class="table table-hover table-striped">
+                <thead>
+                <tr>
+                    <th>Изображение</th>
+                    <th>Название</th>
+                    <th>Количество</th>
+                    <th>Цена</th>
+                    <th>Сумма</th>
+                    <th><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php foreach ($session['cart'] as $id => $item):?>
+                    <tr>
+                        <td><?= Html::img("@web/images/products/{$item['img']}",['height'=>'100','width' => '100']) ?></td>
+                        <td><a href="<?= Url::to(['product/view','id'=>$id])?>"><?= $item['name']?></a></td>
+                        <td><?= $item['qty']?></td>
+                        <td><?= $item['price']?></td>
+                        <td><?= $item['price'] *  $item['qty']?></td>
+                        <td><span class="glyphicon glyphicon-remove text-danger del-item" data-id="<?= $id;?>" aria-hidden="true"></span></td>
+                    </tr>
+                <?php endforeach;?>
+                <tr>
+                    <td colspan="4">Итого: </td>
+                    <td ><?= $session['cart.qty']?></td>
+
+                <tr>
+                    <td colspan="4">На сумму: </td>
+                    <td ><?= $session['cart.sum']?></td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <?php $form = ActiveForm::begin()?>
+        <?= $form->field($order,'name')?>
+        <?= $form->field($order,'email')?>
+        <?= $form->field($order,'phone')?>
+        <?= $form->field($order,'address')?>
+        <?= Html::submitButton('Оформить заказ',['class' =>'btn btn-success'])?>
+        <?php  ActiveForm::end()?>
+    <?php else:?>
+        <h3>Корзина пуста</h3>
+    <?php endif;?>
+
+
+</div>
