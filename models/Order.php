b7d6b1ecc6eba577600ad50b218bcb3d8f0963ac
Genjo <insidia93@gmail.com>
2017-09-28 18:36:12 +0300

    Created behaviors to set time,when client place an order

--- models/Order.php
+++ models/Order.php
@@ -5,6 +5,7 @@ namespace app\models;
 use Yii;
 use yii\db\ActiveRecord;
 use yii\behaviors\TimestampBehavior;
+use yii\db\Expression;
 
 /**
  * This is the model class for table "order".
@@ -36,7 +37,22 @@ class Order extends ActiveRecord
 
     public function getOrderItems()
     {
-        return $this->hasMany(OrderItems::className(),['order_id'=>id]);
+        return $this->hasMany(OrderItems::className(),['order_id'=>'id']);
+    }
+
+    public function behaviors()
+    {
+        return [
+            [
+                'class' => TimestampBehavior::className(),
+                'attributes' => [
+                    ActiveRecord::EVENT_BEFORE_INSERT => ['created_at', 'updated_at'],
+                    ActiveRecord::EVENT_BEFORE_UPDATE => ['updated_at'],
+                ],
+                // если вместо метки времени UNIX используется datetime:
+                 'value' => new Expression('NOW()'),
+            ],
+        ];
     }
 
     public function rules()
