5e539ead281a6d1e978ca50903832be3cb04feae
Genjo <insidia93@gmail.com>
2017-10-02 16:40:53 +0300

    We added the main controller, from which all other controllers will be inherited, of our admin module and set the rules of the login

---
+++ modules/admin/controllers/AppAdminController.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Genjo
+ * Date: 30.09.2017
+ * Time: 18:14
+ */
+
+namespace app\modules\admin\controllers;
+
+
+use yii\web\Controller;
+use yii\filters\AccessControl;
+
+class AppAdminController extends Controller
+{
+
+
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+        ];
+    }
+}
