04d3fee50aea3a0d21cb34b4706bed6c86e804bb
Genjo <insidia93@gmail.com>
2017-10-04 14:25:19 +0300

    Created view to CategoryController in admin panel

---
+++ modules/admin/views/category/view.php
@@ -0,0 +1,45 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+
+/* @var $this yii\web\View */
+/* @var $model app\modules\admin\models\Category */
+
+$this->title = $model->name;
+$this->params['breadcrumbs'][] = ['label' => 'Categories', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="category-view">
+
+    <h1><?= $model->name?></h1>
+    <h1><a href="<?= \yii\helpers\Url::to(['category/index'])?>">Список категорий</a></h1>
+
+    <p>
+        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
+            'class' => 'btn btn-danger',
+            'data' => [
+                'confirm' => 'Are you sure you want to delete this item?',
+                'method' => 'post',
+            ],
+        ]) ?>
+    </p>
+
+    <?= DetailView::widget([
+        'model' => $model,
+        'attributes' => [
+            'id',
+//            'parent_id',
+            [
+                'attribute'=>'parent_id',
+                'value'=> $model->category->name ? $data->category->name : 'Не имеет родительской категории',
+            ],
+
+            'name',
+            'keywords',
+            'description',
+        ],
+    ]) ?>
+
+</div>
